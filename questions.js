86a0b987a7bb867c594ebc0a0a6a89b2fef44254
Neha Sinha <137761848+nsrs-78@users.noreply.github.com>
2024-01-03 01:46:27 +0530

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,41 @@
+let questions=[
+    {    
+        numb: 1,
+        question: 'What is the largest planet in our solar system?',
+        options: ['Mars', 'Saturn', 'Jupiter', 'Neptune'],
+        answer: 'Jupiter', 
+    },    
+    
+    {
+        numb: 2,
+        question: 'Which country won the FIFA World Cup in 2018?',
+        options: ['Brazil', 'Germany', 'France', 'Argentina'],
+        answer: 'France',
+    }, 
+
+    {
+        numb: 3,
+        question: 'What is the tallest mountain in the world?',
+        options: ['Mount Everest', 'K2', 'Kangchenjunga', 'Makalu'],
+        answer: 'Mount Everest',
+    }, 
+
+    {
+        numb: 4,
+        question: 'Which is the largest ocean on Earth?',
+        options: [
+          'Pacific Ocean',
+          'Indian Ocean',
+          'Atlantic Ocean',
+          'Arctic Ocean',
+        ],
+        answer: 'Pacific Ocean',
+    }, 
+    
+    {
+        numb:5 ,    
+        question: 'What is the chemical symbol for gold?',
+        options: ['Au', 'Ag', 'Cu', 'Fe'],
+        answer: 'Au',
+    }, 
+]
